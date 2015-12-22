0961a0ee38652d3838a51672c01ed40359703763
Hannah Krager <hannahkrager@gmail.com>
2015-12-22 13:31:10 -0800

    add script tag for file in index.html, create event listener

--- scripts/estimate.js
+++ scripts/estimate.js
@@ -2,10 +2,20 @@ var estimate = {};
 
 estimate.collectUserInput = function() {
 
-  //define these values from form when submitted
-  //estimate.upFrontCost
-  //estimate.percentReduction
-  //estimate.currentElectricalBill
-  //estimate.madeInWashington = boolean value to see if WA credits apply
-  
+  $('#input-calc').on('submit', function(event){
+    event.preventDefault();
+    //define these values from form when submitted
+    estimate.upFrontCost = $('#up-front-cost').val();
+    estimate.solarPerDay = $('#solar-kwh-per-day').val();
+    estimate.currentElectricalBill = $('#current-kwh-per-day').val();
+    estimate.madeInWashington = $('#made-in-wash:checked') || false;
+    console.log(estimate.madeInWashington);
+  });
+
+
+
 };
+
+$(function(){
+  estimate.collectUserInput();
+});
