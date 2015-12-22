3d24115acaeb9549939b0053ca60d5b562a498a9
Hannah Krager <hannahkrager@gmail.com>
2015-12-21 17:04:39 -0800

    create file and define methods for estimate controller

---
+++ scripts/estimateController.js
@@ -0,0 +1,22 @@
+var estimateController = {};
+
+estimateController.incentiveBenefits = function() {
+  //use this to call all other functions to calculate before going to view
+};
+
+estimateController.calcFederalBenefits = function() {
+  //calculate any federal benefits
+};
+
+estimateController.calcUsageCredits = function(){
+  //calculate usage credits
+};
+
+estimateController.calcStateBenefits = function() {
+  //calculate any WA specific credits if in WA
+};
+
+estimateController.timeToPayOff = function() {
+  //calculate how much time it will take to pay off solar panels
+
+};
