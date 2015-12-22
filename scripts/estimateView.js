62f488e2d13e18fbf491fec1f4292800d34d1370
MaryJo Foster <MJ@MaryJoFoster.com>
2015-12-21 16:56:54 -0800

    Added Initial estimateView.js file

---
+++ scripts/estimateView.js
@@ -0,0 +1,25 @@
+// Declare estimateView object literal
+// All functions for the 'EstimateView' object.
+
+var estimateView = {};
+
+estimateView.renderEstimate = function() {  // Render all calculations for final estimate view
+};
+
+estimateView.renderEstimateTab = function() {  // Render estimate values in lower portion of screen
+};
+
+estimateView.renderTaxIncentives = function() {  // render ALL 'tax benefit' functions, state & federal.
+};
+
+estimateView.stateIncentives = function() {  // render ALL Washington state benefits.
+};
+
+estimateView.NetMeteringIncentive = function() {  //  render WA state 'net metering' benefit.
+};
+
+estimateView.StateManufacturedIncentive = function() {  //  render WA state 'manufactured in WA' incentive.
+};
+
+estimateView.FederalIncentive = function() {  //  render federal benefits.
+};
