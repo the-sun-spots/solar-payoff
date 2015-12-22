e35f56762a08e8570483626862480786bc88092a
Hannah Krager <hannahkrager@gmail.com>
2015-12-22 10:59:45 -0800

    write functions to calculate how many months to pay off solar panels

--- scripts/estimateController.js
+++ scripts/estimateController.js
@@ -1,22 +1,42 @@
 var estimateController = {};
 
 estimateController.incentiveBenefits = function() {
-  //use this to call all other functions to calculate before going to view
+
+  var solarIncome = estimateController.calcUsageCredits();
+  //var newMonthlyEnergyCost = estimateController.adjustedUsage(solarIncome)
+  estimate.PercentOfTotal = estimateController.calcFederalBenefits(estimate.upFrontCost);
+  estimateController.timeToPayOff(solarIncome);
 };
 
-estimateController.calcFederalBenefits = function() {
+estimateController.calcFederalBenefits = function(totalCost) {
   //calculate any federal benefits
+  return totalCost * 0.3;
+
 };
 
 estimateController.calcUsageCredits = function(){
   //calculate usage credits
+  if (estimate.madeInWashington){
+    return solarProduction * 0.54;
+  };
+
+  return solarProduction * 0.15;
 };
 
-estimateController.calcStateBenefits = function() {
-  //calculate any WA specific credits if in WA
-};
+estimateController.adjustedUsage = function(solarIncome) {
+  //current kWh cost per day
+  return estimate.currentElectricalBill - solarIncome;
 
-estimateController.timeToPayOff = function() {
+};
+estimateController.timeToPayOff = function(solarIncome) {
   //calculate how much time it will take to pay off solar panels
+  var toPayOff = estimate.upFrontCost - estimate.PercentOfTotal;
+  var months = 0;
+  var monthlySolarIncome = utility.dayToMonth(solarIncome);
+  while (toPayOff > 0) {
+    toPayOff -= monthlySolarIncome;
+    months += 1;
+
+  }
 
 };
