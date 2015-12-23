015a09ac7d2fa8efce4844c9f3504b7490960bf2
Hannah Krager <hannahkrager@gmail.com>
2015-12-23 12:14:47 -0800

    move logic out of router.js, into appcontroller

--- scripts/appController.js
+++ scripts/appController.js
@@ -1,3 +1,20 @@
+var app = {};
+
+app.setInitialView = function(){
+  $('#new-calc').fadeIn('slow').siblings().hide();
+  $('header').show();
+};
+
+app.focusOnForm = function() {
+  $('#new-calc').fadeIn('slow').siblings().hide();
+  $('header').fadeIn('slow');
+  //add smooth page scroll
+  var goTo = $('#new-calc').offset();
+  $('body').animate({
+    scrollTop: goTo.top
+  }, 1250, 'easeInOutExpo');
+};
+
 $(function(){
   $('[data-toggle="tooltip"]').tooltip();
 });
--- scripts/router.js
+++ scripts/router.js
@@ -1,26 +1,9 @@
-page('/',
-  function() {
-    $('#new-calc').fadeIn('slow').siblings().hide();
-    $('header').show();
-  }
-);
-page('/calculator',
-  function() {
-    $('#new-calc').fadeIn('slow').siblings().hide();
-    $('header').fadeIn('slow');
-    //add smooth page scroll
-    var goTo = $('#new-calc').offset();
-    $('body').animate({
-      scrollTop: goTo.top
-    }, 1250, 'easeInOutExpo');
-  }
-);
-// page('/result');
-page('/about',
-  aboutController.show
-);
-page('/resources',
-  resourcesController.show
-);
+page('/', app.setInitialView);
+
+page('/calculator', app.focusOnForm);
+
+page('/about', aboutController.show);
+
+page('/resources', resourcesController.show);
 
 page.start();
