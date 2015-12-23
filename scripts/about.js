76f3b2be80a248849f350fdfe74692a2c2c0db8f
MaryJo Foster <MJ@MaryJoFoster.com>
2015-12-23 12:51:03 -0800

    Created new token for  GitHub Authentication, connect API, & load 'team' array with Github data

--- scripts/about.js
+++ scripts/about.js
@@ -1,3 +1,29 @@
 var about = {};
 
-//Temporary to get Router funcionality working
+var memberNames = ['MJFoster', 'setheid', 'palindromed', 'jessethach'];  // Array of team member GitHub Logins.
+
+var team = [];  // Array of team member objects.
+
+function TeamMember(memberData) {        // Constructor to create a team member
+  this.memberName = memberData.name;
+  this.memberBio = memberData.bio;
+  this.memberPic = memberData.avatar_url;
+  this.memberHireable = memberData.hireable;
+  this.memberProfile = memberData.html_url;
+};
+
+// Get API data from GitHub & add to newly constructed team object.
+about.createTeamMember = function(memberNames) {
+  memberNames.forEach(function(member, index){
+    $.ajax({
+      url: '/github/users/' + member,
+      type: 'GET',
+      success: function(memberData, message, xhr) {
+        team[index] = new TeamMember(memberData);
+      }
+    });
+  });
+};
+
+// Initiates CREATION & LOADING of 'team' array!
+about.createTeamMember(memberNames);
