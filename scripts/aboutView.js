aa272c15a1a83403add422e6409c089119b5b3e5
Hannah Krager <hannahkrager@gmail.com>
2015-12-26 12:11:15 -0800

    data for each team member renders to about page

--- scripts/aboutView.js
+++ scripts/aboutView.js
@@ -1,3 +1,18 @@
 var aboutView = {};
 
 //Temporary to get Router funcionality working
+aboutView.render = function(team) {
+  $('#about').empty();
+  team.forEach(function(member, index){
+    aboutView.loadTemplate(member);
+  });
+};
+
+aboutView.loadTemplate = function(member) {
+  $.get('/templates/about-template.html')
+   .done(function(data){
+     var template = Handlebars.compile(data);
+     var compiledHtml = template(member);
+     $('#about').append(compiledHtml);
+   });
+};
